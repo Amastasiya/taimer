efea38f4ef7573a8a59c479dea9bdd80c787a605
Anastasia <nastena.maiorova@gmail.com>
2022-12-13 22:11:05 +0300

    Нашли секунды

--- 2022_12_13.py
+++ 2022_12_13.py
@@ -7,22 +7,26 @@
 import datetime
 import time
 
-def now_day():
-    now = datetime.datetime.today()
-    print("Сегодня", now)
+#def now_day():
+    #now = datetime.datetime.today()
+    #print("Сегодня", now)
 
-now = now_day()
+#now = now_day()
 
 def new_year():
     while True:
-        now = datetime.datetime.today()
+        now = datetime.datetime.now()
         #print("Сегодня сейчас", now)
         new_year = datetime.datetime(2023, 1, 1)
         counter_time = new_year - now
+        print(counter_time.days*24*60*60 + counter_time.seconds) # секунды
         time.sleep(5)
         print("Осталось до Нового года", counter_time)
 
 new_year()
+
+
+
 
 
 
