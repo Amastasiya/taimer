7e60900fe5c9099d84585fd1437e7307b43b75f9
Anastasia <nastena.maiorova@gmail.com>
2022-12-13 20:29:19 +0300

    создали дату сегодня и дату нового года

---
+++ 2022_12_13.py
@@ -0,0 +1,20 @@
+# Сделать таймер отсчета времени до нового года
+
+# 1 Взять библиотеку datetime
+# 2 с помощью этой библиотеке получить текущую дату и время
+# 3 Вычислить количество секунд до НГ вывести в консоле
+
+import datetime
+
+now = datetime.datetime.today()
+print("Сегодня", now)
+
+second = now.second
+print(second)
+
+new_year = datetime.datetime(2023, 1, 1)
+print(new_year)
+
+
+
+
