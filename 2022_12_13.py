5f3f4d86a233015a01a4275729a73e49a38bcb03
Anastasia <nastena.maiorova@gmail.com>
2022-12-13 21:32:02 +0300

    Для работы оставляем функцию new_year, в нее добавили while и time.sleep() для обновления счетчика

--- 2022_12_13.py
+++ 2022_12_13.py
@@ -5,18 +5,28 @@
 # 3 Вычислить количество секунд до НГ вывести в консоле
 
 import datetime
+import time
 
 def now_day():
     now = datetime.datetime.today()
-    print("Сегодня сейчас", now)
-    return now
+    print("Сегодня", now)
+
 now = now_day()
 
-def new_year_second(now):
-    new_year = datetime.datetime(2023, 1, 1)
-    print(new_year)
-    counter_time = new_year - now
-    return counter_time
+def new_year():
+    while True:
+        now = datetime.datetime.today()
+        #print("Сегодня сейчас", now)
+        new_year = datetime.datetime(2023, 1, 1)
+        counter_time = new_year - now
+        time.sleep(5)
+        print("Осталось до Нового года", counter_time)
+
+new_year()
+
+
+
+
 
 
 
